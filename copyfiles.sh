eac5445fa81560cb619e1e85456c8ea366de8fa0
Doug Toppin <dougtoppin@gmail.com>
2018-01-21 21:16:28 -0500

    adding rds support, not complete yet

--- copyfiles.sh
+++ copyfiles.sh
@@ -1,6 +1,6 @@
 PROFILE=
 LOC=s3://dougtoppin-cloudformation/ecs-refarch-cloudformation
-LIST="master.yaml infrastructure/ecs-cluster.yaml infrastructure/security-groups.yaml infrastructure/load-balancers.yaml infrastructure/vpc.yaml services/product-service/service.yaml services/website-service/service.yaml services/springboot-service/service.yaml services/springboot-service1/service.yaml services/springboot-service2/service.yaml infrastructure/rds.json"
+LIST="master.yaml infrastructure/ecs-cluster.yaml infrastructure/security-groups.yaml infrastructure/load-balancers.yaml infrastructure/vpc.yaml services/product-service/service.yaml services/website-service/service.yaml services/springboot-service1/service.yaml services/springboot-service2/service.yaml infrastructure/rds.json"
 
 for i in $LIST
 do
--- start.sh
+++ start.sh
@@ -1,18 +1,23 @@
 LISTENER=
-VPC=
+VPC=vpc-e1323a99
 STACK1=test-ecs-15
 STACK2=test-ecs-15-springboot1
 STACK3=test-ecs-15-springboot2
 REPO=~/Desktop/Projects/Github/ecs-refarch-cloudformation
 URL=https://s3.amazonaws.com/dougtoppin-cloudformation/ecs-refarch-cloudformation/master.yaml
-SUBNETS=""
+SUBNETS="subnet-50563b7f,subnet-d2250f99"
+ECSSG="sg-07f2f773"
 BODY=
 
 PROFILE=
 
+usage() {
+    echo "Usage: $0 stack|boot1|boot2|rds"
+}
+
 if test $# -ne 1
 then
-    echo "Usage: $0 resource"
+    usage
     exit 1
 fi
 
@@ -30,10 +35,10 @@ case $1 in
        aws cloudformation create-stack --stack-name $STACK3 --template-url https://s3.amazonaws.com/dougtoppin-cloudformation/ecs-refarch-cloudformation/services/springboot-service2/service.yaml --parameters ParameterKey=DesiredCount,ParameterValue=2 ParameterKey=VPC,ParameterValue=$VPC ParameterKey=Path,ParameterValue=/springboot2 ParameterKey=Listener,ParameterValue=$LISTENER ParameterKey=Cluster,ParameterValue=$STACK1 --capabilities CAPABILITY_NAMED_IAM
         ;;
     rds)
-       aws cloudformation create-stack --stack-name "$STACK1"-rds --template-url https://s3.amazonaws.com/dougtoppin-cloudformation/ecs-refarch-cloudformation/infrastructure/rds.json --parameters ParameterKey=Subnets,ParameterValue=\"$SUBNETS\" ParameterKey=VpcId,ParameterValue=$VPC
+       aws cloudformation create-stack --stack-name "$STACK1"-rds --template-url https://s3.amazonaws.com/dougtoppin-cloudformation/ecs-refarch-cloudformation/infrastructure/rds.json --parameters ParameterKey=Subnets,ParameterValue=\"$SUBNETS\" ParameterKey=VpcId,ParameterValue=$VPC ParameterKey=ECSSecurityGroupID,ParameterValue=\"$ECSSG\"
         ;;
     *)
-      echo "Usage: $0 resource"
+      usage
       exit 1
 esac
 
